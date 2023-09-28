e54eac1e38fc20f0a77d7bec826e6f572df5d8e8
bllnr <52850477+bllnr@users.noreply.github.com>
2023-09-28 20:27:43 +0200

    Creating mathScripts repository

--- EuklidesAlgo.py
+++ EuklidesAlgo.py
@@ -1,14 +1,23 @@
 #Euklides Algorithm
 #Find the greatest common divisor of two numbers
 
-input_a = input("input first number:\n")
-input_b = input("input second number:\n")
+input_a = input("Input first number:\n")
+input_b = input("Input second number:\n")
 
-#input_a=3042
-#input_b=4485
+#input_a=876
+#input_b=204
 
-a = int(input_a)
-b = int(input_b)
+try:
+    a = int(input_a)
+    b = int(input_b)
+except ValueError:
+    print("Input must be number!")
+    exit()
+
+if input_a == 0 or input_b == 0:
+    print("Number can't be zero. Exiting.")
+    exit()
+
 a, b = max(a,b), min(a,b)
 
 def find_q(a,b):
@@ -21,9 +30,12 @@ def find_r(a,b,q):
 
 gcd = 1
 r = 1
+results = []
 #find the greatest common divisor recursively
 while r > 0:
     r = find_r(a,b,find_q(a,b))
+    j = [a,b,find_q(a,b),r]
+    results.append(j)
     a = b
     b = r
     if r != 0:
@@ -34,6 +46,4 @@ while r > 0:
 print(f"Greatest common divisor of {input_a} and {input_b} is {gcd}.")
 
 if gcd == 1:
-    print("Mutually prime!")
-
-
+    print("Mutually prime!")    
